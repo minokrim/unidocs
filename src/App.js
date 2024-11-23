0434ef9dd4b82ec07b43de087c8ee3218dd6dd23
glidi1 <glidi1998@gmail.com>
2024-11-23 21:34:27 +0000

    supp

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
-
-
-
-import './App.css';
-
+import logo from './logo.svg';
+import '../App.css';
+import Home from './home/home';
 import NavDash from './dashboard/navdash';
 function App() {
   return (
    <div>
     <NavDash />
     
+
    </div>
-
-import logo from './logo.svg';
-
-import './App.css';
-import Home from './home/home';
-import React, { useEffect, useState } from 'react';
-import axios from "axios"
-
-export const userContext=React.createContext();
-
-
-function App() {
-
-  const [user,setuser]=useState(null)
-
-  useEffect(()=>{
-    axios.get("http://localhost:5000/session",{withCredentials:true})
-    .then((response)=>{
-      setuser(response.data.email);
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
-  },[])
-
-  return (
-
-    <userContext.Provider value={user}>
-          <Home/>
-    </userContext.Provider>
-
-    <Home/>
-
-
-  );
+  )
 }
 
-export default App;
