3f0ee03c59965ea97363736887f1f81f5ce4e302
ayomide <ayomidekareem563@gmail.com>
2024-11-23 16:18:45 -0800

    update on both merge

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-<<<<<<< HEAD
-
-
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import Dashboard from './dashboard/dashboard';
-=======
 import App from './App';
 import reportWebVitals from './reportWebVitals';
->>>>>>> baa9fa1f00db3c75549f4c06312ce72859ee2f1e
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-  <Dashboard />
-=======
     <App />
->>>>>>> baa9fa1f00db3c75549f4c06312ce72859ee2f1e
   </React.StrictMode>
 );
 
