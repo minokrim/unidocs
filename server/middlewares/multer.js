fbf32da32b6569badcb295f5c30609879f905d89
ayomide <ayomidekareem563@gmail.com>
2025-09-01 09:12:36 -0700

    mem redo

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -2,14 +2,8 @@ import multer from 'multer';
 import path from 'path';
 
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');  
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));  
-    }
-});
+const storage = multer.memoryStorage();
 
-const upload = multer({ storage: storage },)
+const upload = multer({ storage });
+
 export default upload;
