dfd04adcdad6dae47ed144c5cf53ca5d5935ca75
ayomide <ayomidekareem563@gmail.com>
2025-09-01 09:01:07 -0700

    correct supabase credentails

--- server/config/db.js
+++ server/config/db.js
@@ -9,8 +9,8 @@ dotenv.config();
 const { Client } = pg;
 
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = process.env.supabaseUrl;
+const supabaseKey = process.env.supabaseKey;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
