836c1a090cf54179945bdda363e24055bf939597
ayomide <ayomidekareem563@gmail.com>
2025-09-01 05:59:15 -0700

    cors url fix

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const upload = multer({ storage: storage },)
 const app =express();
 const PORT=process.env.SERVER_PORT
 app.use(cors({
-    origin: 'https://unidocs-ukv1.onrender.com',  
+    origin: 'https://unidocs-1.onrender.com',  
     credentials: true   ,
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']             
