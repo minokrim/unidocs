24dd0d4f6fa4f68aa565f9a40c5eef73597b0355
ayomide <ayomidekareem563@gmail.com>
2025-09-15 20:27:31 -0700

    debug 4

--- server/server.js
+++ server/server.js
@@ -33,19 +33,12 @@ const upload = multer({ storage: storage },)
 
 const app =express();
 const PORT=process.env.SERVER_PORT
-const corsOption={
-    origin: 'https://unidocs-1.onrender.com',  
-    credentials: true   ,
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']             
-  }
 app.use(cors({
-    origin: '*',  
+    origin: 'https://unidocs-1.onrender.com/',  
     credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']             
   }));
-app.options("*", cors(corsOption)); 
 
 app.use(passport.initialize());
 const __dirname = dirname(fileURLToPath(import.meta.url));
